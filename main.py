052f6657b03ba1ebf893e1ee407bc5466a8ae022
theglitchmaster64 <glitch@predator.localdomain>
2023-09-28 18:10:31 +0530

    got historical data from 21june2020 to 21june2023;todo - improve candle fetchers; implement analysis/backtesting & execution

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ SECRET_KEY = secrets['SECRET_KEY']
 API_SESSION = secrets['API_SESSION']
 
 
+
 if __name__ == '__main__':
     print(wasup())
     breeze = BreezeConnect(api_key=API_KEY)
@@ -19,3 +20,60 @@ if __name__ == '__main__':
         print('session begun at: {}'.format(datetime.datetime.now().strftime("%d:%m:%y:%H:%M:%S")))
     except Exception as e:
         print('failed to generate session: {}'.format(e))
+
+
+    try:
+        breeze.ws_connect()
+        print('websocket connected successfully!')
+    except Exception as e:
+        print('websocket failed: ',e)
+
+
+    breeze.on_ticks = on_tick
+
+    # todo;
+    def test_subscribe():
+        breeze.subscribe_feeds(exchange_code="NSE",
+        stock_code="NIFTY",
+        product_type="futures",
+        expiry_date="",
+        strike_price="",
+        right="",
+        get_exchange_quotes=True,
+        get_market_depth=False)
+
+    def test_unsubscribe():
+        breeze.unsubscribe_feeds(exchange_code="NSE",
+        stock_code="NIFTY",
+        product_type="futures",
+        expiry_date="",
+        strike_price="",
+        right="",
+        get_exchange_quotes=True,
+        get_market_depth=False)
+
+    def histDataFetcher(from_date,interval, ticker,exchange):
+        intervals_dict = {"1second":1,"1minute":60,"5minute":300,"30minute":1800,"1day":86400}
+        MAX_CANDLES = 1000
+        now = datetime.datetime.now()
+        fromDateObj = convDate_str2obj(from_date)
+        candlesToGet = (now-fromDateObj).total_seconds() // intervals_dict[interval]
+        flag = 0
+        final_results = []
+        while candlesToGet > 0:
+            result = breeze.get_historical_data_v2(interval=interval,
+            from_date = convDate_obj2iso8601(fromDateObj),
+            to_date = convDate_obj2iso8601(now),
+            stock_code = "NIFTY",
+            exchange_code = "NSE",
+            product_type = "",
+            expiry_date = "",
+            right = "",
+            strike_price = "")
+            if len(result['Success']) == 0:
+                print('0 candles was returned!')
+                return None
+            candlesToGet -= len(result['Success'])
+            fromDateObj += datetime.timedelta(seconds=1000*intervals_dict[interval])
+            final_results += result['Success']
+        return final_results
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import urllib.parse
+import datetime
+import pandas as pd
 
 def wasup():
     return 'greetings friend ;)'
@@ -16,3 +18,47 @@ def load_config(filename: str, split_by='='):
         a, b = secret.split(split_by)
         dict[a] = b
     return dict
+
+#breeze callback
+def on_tick(ticks):
+    print("\ntick: {}\n".format(ticks))
+
+def convDate_str2obj(input_date):
+    return datetime.datetime.strptime(input_date, "%d:%m:%Y:%H:%M:%S")
+
+def convDate_obj2iso8601(input_date):
+    return (input_date - datetime.timedelta(hours=5, minutes=30)).strftime("%Y-%m-%dT%H:%M:%S") + '.000Z'
+
+def options_getHistData(breezeObj, interval, tickerSym, exchangeCode, fromDate, toDate, expDate, right, strikePrice):
+    intervals_dict = {"1second":1,"1minute":60,"5minute":300,"30minute":1800,"1day":86400}
+    timeA = datetime.datetime.strptime(fromDate, "%d:%m:%Y:%H:%M:%S")
+    timeB = datetime.datetime.strptime(toDate, "%d:%m:%Y:%H:%M:%S")
+    candles_rem = 0
+    if timeA > timeB:
+        raise Exception('error: fromDate is later than toDate!')
+    deltaTime = timeB - timeA
+    num_candles = deltaTime.total_seconds() // intervals_dict[interval]
+    if num_candles < 1:
+        raise Exception('error: Date/Time range too small!!')
+    elif num_candles > 1000:
+        candles_rem = num_candles -1000
+        print('total candles requested: ',num_candles)
+    try:
+        result = breezeObj.get_historical_data(interval,
+            from_date=convDate(fromDate),to_date=convDate(toDate),
+            stock_code=tickerSym,
+            exchange_code=exchangeCode,
+            product_type='options',
+            expiry_date=expDate,
+            right=right,
+            strike_price=strikePrice)
+        if len(result['Success']) == 0:
+            print('failed to retrieve data')
+            print(result)
+        else:
+            return (result['Success'],candles_rem)
+    except Exception as err:
+        print('failed to get data with error: ',err)
+        return result
+
+#breeze.get_historical_data_v2(interval="1minute", from_date= "2022-08-15T07:00:00.000Z",to_date= "2022-08-17T07:00:00.000Z", stock_code="NIFTY", exchange_code="NSE",product_type="options",expiry_date="2022-09-29T07:00:00.000Z", right="call", strike_price="")
